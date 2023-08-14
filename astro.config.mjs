50499b442947fc90da2da650fd3fabb99466b9a5
ZorkDaNerd <zork9898@colostate.edu>
2023-08-13 20:11:00 -0600

    fixed cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare() //https://developers.cloudflare.com/pages/framework-guides/deploy-an-astro-site/
+  adapter: cloudflare(), //https://developers.cloudflare.com/pages/framework-guides/deploy-an-astro-site/
+  site: 'https://zachshimpa.com/'
 });
