ff6bc2f880f2137e6e84e7f8547329e77b015d95
ZorkDaNerd <zork9898@colostate.edu>
2023-10-04 20:17:39 -0600

    Cleaned repo, re initialized

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: cloudflare(), //https://developers.cloudflare.com/pages/framework-guides/deploy-an-astro-site/
-  site: 'https://zachshimpa.com/'
-});
+export default defineConfig({});
