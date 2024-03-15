083844872ac3359662cf1c381d74d61a47365cea
ZorkDaNerd <zork9898@colostate.edu>
2024-03-15 00:56:04 -0600

    Added astro db, and astro update 4.3.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://zachshimpa.com',
-  })
+  site: 'https://zachshimpa.com',
+  integrations: [db()]
+});
